f668f52e9a1e7d9bc379b6f346fd306f0694fca8
enbohm <andreas@enbohms.se>
2023-04-21 08:20:04 +0200

    Adding tests

---
+++ src/components/HelloWorld.spec.js
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import HelloWorld from './HelloWorld.vue'
+
+describe('HelloWorld', () => {
+  it('should display header text', () => {
+    const msg = 'Hello Vue 3'
+    const wrapper = mount(HelloWorld, { props: { msg } })
+
+    expect(wrapper.find('h1').text()).toEqual(msg)
+  })
+})

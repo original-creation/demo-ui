9926b7f3e2262626a0dc1db38d848f8c58eb5582
enbohm <andreas@enbohms.se>
2023-04-24 15:51:18 +0200

    Fixing build errors

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import DemoApp from './components/DemoApp.vue';
 import Navbar from './components/Navbar.vue'
-import { ref } from 'vue';
-
-const demoOn = ref(false);
 
 </script>
 
 <template>
-  <div v-show="!demoOn">
     <header id="nav-wrapper">
       <Navbar />
     </header>
     <main id="main">
       <router-view />
     </main>
-  </div>
-  <div v-show="demoOn">
-    <DemoApp/>
-  </div>
 </template>
